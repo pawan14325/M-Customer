408005fd6cac224ba0cb4c93b0206781d9cb8e3c
rowthtechs <mukeshbhandari@rowthtech.com>
2024-08-30 18:56:23 +0530

    home changes

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:m_customer/routes/app_routes.dart';
+
+class AppPages{
+  static const String initialRoute=AppRoutes.splash;
+  static  List routes=[
+    GetPage(
+   transitionDuration: Duration(milliseconds: 900),
+  name: AppRoutes.splash, page: ()=>),
+  ];
+}
