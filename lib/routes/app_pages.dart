d409af44205cc2b5a6f46ecc0c7d8f9b7654b7fc
rowthtechs <mukeshbhandari@rowthtech.com>
2024-09-03 10:42:39 +0530

    strc

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -4,8 +4,8 @@ import 'package:m_customer/routes/app_routes.dart';
 class AppPages{
   static const String initialRoute=AppRoutes.splash;
   static  List routes=[
-    GetPage(
-   transitionDuration: Duration(milliseconds: 900),
-  name: AppRoutes.splash, page: ()=>),
+  //   GetPage(
+  //  transitionDuration: Duration(milliseconds: 900),
+  // name: AppRoutes.splash, page: ()=>),
   ];
 }
